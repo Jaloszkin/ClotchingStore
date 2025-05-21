effdff6c676c021185b70deeedf96fd19210f727
Super <Super@DESKTOP-05U8FMD>
2025-05-21 21:03:39 +0300

    laaaasst

--- ClothingStore/settings.py
+++ ClothingStore/settings.py
@@ -125,3 +125,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    'clotchingstore.onrender.com',  # Ваш домен на Render
+    'localhost',
+    '127.0.0.1',
+    'ping.uptimerobot.com'   # Для UptimeRobot (без кавычек в конце строки!)
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'clotchingstore.onrender.com',
+]
